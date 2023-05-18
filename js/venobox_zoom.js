52f84611b2e6037c7a59a127d497777a32497851
ihan.chen <ihan.chen@udnshopping.com>
2023-05-18 11:49:26 +0800

    style: 產品venobox

---
+++ js/venobox_zoom.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".zoomVideo").venobox({
+        closeColor: "#fff",
+        spinner: "three-bounce",
+        spinColor: "#0097da"
+    });
+
+    $(".venobox").venobox({
+        closeColor: "#fff",
+        spinner: "three-bounce",
+        spinColor: "#0097da"
+    });
+
+    $(".venobox-login").venobox({
+        framewidth: "75%",
+        frameheight: "auto",
+        border: "10px",
+        closeColor: "#fff",
+        spinner: "three-bounce",
+        spinColor: "#0097da"
+    });
+
+    $(".venobox-qrcode").venobox({
+        framewidth: "250px",
+        frameheight: "auto",
+        border: "10px",
+        closeColor: "#fff",
+        spinner: "three-bounce",
+        spinColor: "#0097da"
+    });
+});
